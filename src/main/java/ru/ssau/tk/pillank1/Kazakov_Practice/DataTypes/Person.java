52e1459aba1e1e66aeeef0e89632fbcc91f1c3cc
Победитель <nikita37pobeditel@mail.ru>
2021-01-12 23:33:39 +0400

    1.1 + настройка pom.xml

---
+++ src/main/java/ru/ssau/tk/pillank1/Kazakov_Practice/DataTypes/Person.java
@@ -0,0 +1,30 @@
+package ru.ssau.tk.pillank1.Kazakov_Practice.DataTypes;
+
+public class Person {
+    private String firstName, lastName;
+    private int passportId;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getPassportId() {
+        return passportId;
+    }
+}
