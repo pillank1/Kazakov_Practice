6b4763a7939d1089de2d0af2699d99e33f176d42
Победитель <nikita37pobeditel@mail.ru>
2021-01-13 03:40:16 +0400

    1.11

--- src/test/java/ru/ssau/tk/pillank1/Kazakov_Practice/DataTypes/Person/PersonTest.java
+++ src/test/java/ru/ssau/tk/pillank1/Kazakov_Practice/DataTypes/Person/PersonTest.java
@@ -28,4 +28,40 @@ public class PersonTest {
         assertEquals(man.getPassportId(), 646551);
         assertEquals(woman.getPassportId(), 155646);
     }
+
+    Person noName = new Person();
+    Person womanNoPassport = new Person("Anastasia", "Ausheva");
+    Person passportOfNoName = new Person(null, null, 564684);
+    Person korbanDallas = new Person(578155);
+
+    @Test
+    public void testFirstNameMethods() {
+        noName.setFirstName("Egor");
+        womanNoPassport.setFirstName(null);
+        passportOfNoName.setFirstName("Pavel");
+        assertEquals(noName.getFirstName(), "Egor");
+        assertNull(womanNoPassport.getFirstName());
+        assertEquals(passportOfNoName.getFirstName(), "Pavel");
+        assertNull(korbanDallas.getFirstName());
+    }
+
+    @Test
+    public void testLastNameMethods() {
+        passportOfNoName.setLastName("Aleksandrov");
+        korbanDallas.setLastName(null);
+        assertNull(noName.getLastName());
+        assertEquals(womanNoPassport.getLastName(), "Ausheva");
+        assertEquals(passportOfNoName.getLastName(), "Aleksandrov");
+        assertNull(korbanDallas.getLastName());
+    }
+
+    @Test
+    public void testPassportIDMethods() {
+        womanNoPassport.setPassportId(945186);
+        korbanDallas.setPassportId(988774);
+        assertEquals(noName.getPassportId(), 0);
+        assertEquals(womanNoPassport.getPassportId(), 945186);
+        assertEquals(passportOfNoName.getPassportId(), 564684);
+        assertEquals(korbanDallas.getPassportId(), 988774);
+    }
 }
