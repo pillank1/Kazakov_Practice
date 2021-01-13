c05ac0784e0105441a85ee64d59e487092db3249
Победитель <nikita37pobeditel@mail.ru>
2021-01-13 10:42:56 +0400

    1.16

---
+++ src/main/java/ru/ssau/tk/pillank1/Kazakov_Practice/DataTypes/NamedPoint.java
@@ -0,0 +1,29 @@
+package ru.ssau.tk.pillank1.Kazakov_Practice.DataTypes;
+
+import ru.ssau.tk.pillank1.Kazakov_Practice.DataTypes.point.Point;
+
+public class NamedPoint extends Point {
+    private String name;
+
+    public NamedPoint(double x, double y, double z) {
+        super(x, y, z);
+    }
+
+    public NamedPoint(double x, double y, double z, String name) {
+        super(x, y, z);
+        this.name = name;
+    }
+
+    public NamedPoint() {
+        this(0, 0, 0, "Origin");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
