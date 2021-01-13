dabca4f387bdd750823a0c67c48c616108fdd0f5
Победитель <nikita37pobeditel@mail.ru>
2021-01-13 09:32:02 +0400

    1.14

--- src/main/java/ru/ssau/tk/pillank1/Kazakov_Practice/DataTypes/Person/ClassForChecking.java
+++ src/main/java/ru/ssau/tk/pillank1/Kazakov_Practice/DataTypes/Person/ClassForChecking.java
@@ -4,30 +4,22 @@ public class ClassForChecking {
     public static void main(String[] args) {
         Person person = new Person();
         person.setFirstName("Arkadiy");
-        int number = 5;
-        checkInt(number);
-        checkPerson(person);
-        System.out.println(number);
+        checkAnotherPerson(person);
         System.out.println(person.getFirstName());
     }
 
-    private static void checkInt(int number) {
-        number = 10;
-        System.out.println(number);
-    }
-
-    private static void checkPerson(Person person) {
-        person.setFirstName("Oleg");
+    private static void checkAnotherPerson(Person person) {
+        person = new Person();
+        person.setFirstName("Ignat");
         System.out.println(person.getFirstName());
     }
+
 /*
 Вывод в консоль:
-10
-Oleg
-5
-Oleg
-Первым делом выполняется метод checkInt(). Аргумент number - аргумент-примитив, что означает, что его можно редактировать. Это и было сделано внутри метода.
-Далее выполняется метод checkPerson(), в котором передается аргумент ссылочного типа. Это приводит к тому, что после выполнения метода значение ссылки объекта person изменится.
-Затем main выведет number = 5, т.к. копия с number = 10 осталась в методе checkInt(), а имя выведет Oleg, потому что вызванный ранее метод checkPerson() поменял значение ссылки объекта person.
+Ignat
+Arkadiy
+Поначалу идет выполнение метода checkAnotherPerson(). Он создает новый объект person, при этом присваивает ему ссылку внутри метода.
+Затем в методе передается имя (Ignat) и выводится в консоль. После выполнения метода сборщик мусора уничтожает ссылку на созданный объект.
+Потом выводится имя (Arkadiy), которое было определено в main для объекта person.
  */
 }
